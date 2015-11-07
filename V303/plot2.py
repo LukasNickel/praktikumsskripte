d40e78f70fa622bf0055efb923ec7b57c22f2e1c
LukasNickel <lukasnickel96@googlemail.com>
2015-11-07 16:48:16 +0100

    Worked on Content, matplotlib is giving errors - ghostscript 7.07 or later needs to be installed

--- V303/plot2.py
+++ V303/plot2.py
@@ -1,12 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 phi, u, low, gain = np.genfromtxt('data\data2.txt', unpack=True)
 
 plt.plot(phi, u, 'rx')
 plt.ylabel('$U /$ V')
 plt.xlabel('$\phi$ / deg')
-plt.savefig('plot2u.pdf')
+plt.tight_layout(pad=0)
+plt.savefig('plot2u.pdf', bbox_inches='tight', pad_inches=0)
+
 
 plt.clf()
 
